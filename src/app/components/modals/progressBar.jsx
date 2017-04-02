41202b4ef898802807f6e3b43f3a2e2370981546
Franklin Jeng <franklinjjeng@gmail.com>
2017-04-02 20:31:17 +0900

    (cleanup) removing unused files

--- src/app/components/modals/progressBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import LinearProgress from 'material-ui/LinearProgress';
-
-const ProgressBar = () => (
-  // <LinearProgress mode="indeterminate" />
-  <div className="progress-bar"></div>
-);
-
-export default ProgressBar;
