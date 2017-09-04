cdb0b9cfc977b7f0f14d1b2fdc0a40ab9cc2a2b9
Franklin Jeng <franklinjjeng@gmail.com>
2017-09-04 15:53:29 -0700

    (feat) adding content to contact page

--- src/app/components/contact.jsx
+++ src/app/components/contact.jsx
@@ -5,11 +5,40 @@ export default class Contact extends React.Component {
   render() {
     return (
       <div className="contact">
-        <div className="coming-soon">
+        {/*<div className="coming-soon">
           coming soon: contact and resume
+        </div>*/}
+        <div className="contact_info">
+          <span className="contact_name">
+            Franklin Jeng
+          </span>
+          <br/>
+          <span className="contact_details">
+            San Jose, CA
+          </span>
+          <br/>
+          <span className="contact_details">
+            franklinjjeng@gmail.com
+          </span>
+          <br/>
+          <span className="contact_details">
+            <a href="https://www.linkedin.com/in/franklinjjeng">
+              https://www.linkedin.com/in/franklinjjeng
+            </a>
+          </span>
+          <br/>
+          <span className="contact_details">
+            <a href="https://github.com/franklinjjeng/franklinjeng">
+              https://github.com/franklinjjeng/franklinjeng
+            </a>
+          </span>
+          <div className="resume">
+            <a className="download_img" href="/assets/JengFranklinResume.pdf" target="_blank">
+              <img className="resume_img" src="/assets/img/JengFranklinResume.jpg"></img>
+            </a>
+          </div>
         </div>
       </div>
     );
   }
 }
-
