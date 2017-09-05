906d0b2e694561f0a3d1f7ad8a80f99ddf0bee35
Franklin Jeng <franklinjjeng@gmail.com>
2017-09-04 18:04:48 -0700

    (feat) adding footer content

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -5,7 +5,25 @@ export default class Footer extends React.Component {
   render() {
     return (
       <div className="footer">
-          <span className="footer_text">Built with &#10084; in San Francisco</span>
+        <span className="footer_text">&#169; 2017 Franklin Jeng</span>
+
+        <div className="footer_links">
+          <div className="footer_icons">
+            <a className="mail" href="mailto:franklinjjeng@gmail.com" target="_blank">
+              <img src="/assets/img/footer/mail-32.ico"/>
+            </a>
+          </div>
+          <div className="footer_icons">
+            <a className="github" href="https://github.com/franklinjjeng">
+              <img src="/assets/img/footer/github-6-32.ico"/>
+            </a>
+          </div>
+          <div className="footer_icons">
+            <a className="linkedIn" href="https://www.linkedin.com/in/franklinjjeng">
+              <img src="/assets/img/footer/linkedin-3-32.ico"/>
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
