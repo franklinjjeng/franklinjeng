9a56de57f3088f8714bd4c83da13505e1c4d56c1
Franklin Jeng <franklinjjeng@gmail.com>
2017-09-11 14:12:38 -0700

    (cleanup) updating naming scheme to match standards adding compression for webpack

--- src/app/client.jsx
+++ src/app/client.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory, Redirect } from 'react-router';
 
-import AboutMe from './components/aboutMe/aboutMe.jsx';
-import App from './components/app.jsx';
-import Contact from './components/contact/contact.jsx';
-import Footer from './components/footer/footer.jsx';
-import Home from './components/home/home.jsx';
-import NoPage404 from './components/noPage404.jsx';
-import Projects from './components/projects/projects.jsx';
+import AboutMe from './components/aboutMe/AboutMe.jsx';
+import App from './components/App.jsx';
+import Contact from './components/contact/Contact.jsx';
+import Footer from './components/footer/Footer.jsx';
+import Home from './components/home/Home.jsx';
+import NoPage404 from './components/NoPage404.jsx';
+import Projects from './components/Projects/projects.jsx';
 
 function createElement(Component, props) {
   return <Component key={`RouteComponent-${props.location.pathname}`} {...props} />;
