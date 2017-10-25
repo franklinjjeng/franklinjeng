2ce8ec6f35f487604b43c39cf398caa2e1d6a676
Franklin Jeng <franklinjjeng@gmail.com>
2017-10-25 15:09:25 -0700

    (update) removing contact information

--- src/app/components/contact/contact.jsx
+++ src/app/components/contact/contact.jsx
@@ -5,10 +5,10 @@ export default class Contact extends React.Component {
   render() {
     return (
       <div className="contact">
-        {/*<div className="coming-soon">
-          coming soon: contact and resume
-        </div>*/}
-        <div className="contact_header">Get In Touch: </div>
+        <div className="coming-soon">
+          Removing my resume for the time being. Not currently looking for a new position at the moment.
+        </div>
+        {/*<div className="contact_header">Get In Touch: </div>
         <div className="contact_content">
           <div className="contact_info">
             <span className="contact_name">
@@ -40,7 +40,7 @@ export default class Contact extends React.Component {
               <a className="download-link" href="https://assets.franklinjeng.com/contact/JengFranklinResume.pdf" target="_blank">Download Resume</a>
             </div>
           </div>
-        </div>
+        </div>*/}
 
       </div>
     );
