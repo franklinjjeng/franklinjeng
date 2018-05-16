5aa5d81a267688c93349f560c9ecafcf9f2bd556
Franklin Jeng <franklinjeng@Franklin-WELL.local>
2018-05-16 15:58:32 -0700

    (feat) adding window resize toggle for full and collapsed nav

--- src/app/components/navigation/Navbar.jsx
+++ src/app/components/navigation/Navbar.jsx
@@ -6,14 +6,32 @@ import NavMenuFull from './NavMenuFull.jsx';
 import NavMenuCollapse from './NavMenuCollapse.jsx';
 
 export default class NewNavbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { width: 0 };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth });
+  }
 
   render() {
     return (
       <div className="nav-bar">
         <div className="nav-content">
           <Link className="nav-button nav-home" to="/">FJeng</Link>
-          <NavMenuFull />
-          {/* <NavMenuCollapse /> */}
+          {this.state.width}
+          {this.state.width > 700 ? <NavMenuFull /> : <NavMenuCollapse />}
         </div>
       </div>
     );
